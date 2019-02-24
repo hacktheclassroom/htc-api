c9707e939ba016002befafa6341b74e8518c61aa
vesche <austin.jackson@swimlane.com>
2019-02-23 17:18:51 -0700

    Update API

--- htc_api/api.py
+++ htc_api/api.py
@@ -15,11 +15,25 @@ class Client:
         self.session = requests.Session()
         self.session.headers['Content-Type'] = 'application/json'
 
+    def _request(self, method, uri, payload=None):
+        response = self.session.request(method, self.base_url + uri, json=payload)
+        response.raise_for_status()
+        return response.json()
+
+    def validate(self, server_code=None):
+        server_code = self.server_code if not server_code else server_code
+        payload = {
+            'server_code': self.server_code
+        }
+        return self._request('GET', 'validate', payload)
+
+    def score(self, username=None):
+        username = self.username if not username else username
+        return self._request('GET', 'score/' + username)
+
     def solve(self, level_id, flag):
         payload = {
             'level_id': level_id,
             'flag': flag
         }
-        r = self.session.request('POST', self.base_url + 'solve', json=payload)
-        r.raise_for_status()
-        return r.json()
+        return self._request('GET', 'solve', payload)
